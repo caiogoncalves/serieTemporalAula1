1366747f8a301774a19282613ed55be1acb66f7d
Caio Goncalves <caio.goncalves@gmail.com>
2018-06-09 10:38:14 -0300

    inserido a reg multipla

--- Aula1.R
+++ Aula1.R
@@ -1,10 +1,25 @@
 
 #Define o dataFrame da regressão
-st <- data.frame(tempo = c(1,2,3,4,5,6), result = c(14,16,19,22,24,26))
+st <- data.frame(tempo = c(14,16,19,22,24), 
+                 result = c(16,19,22,24,26))
 #define o data frame da projeção
-proj <- data.frame(tempo = c(7), result = "")
+proj <- data.frame(tempo = c(26), result = "")
 
 ### cira o objeto da projeção
 reglin <- lm(result ~ tempo, data = st)
 #prediz o valor do objeto
 round(predict(proj,object = reglin), 2)
+summary(reglin)
+
+#Define o dataFrame da regressão
+st <- data.frame(x1 = c(14,16,19,22), 
+                 x2 = c(16,19,22,24),
+                 y = c(19,22,24,26))
+#define o data frame da projeção
+proj <- data.frame(x1 = c(24),x2 = c(26), y = "")
+
+### cira o objeto da projeção
+reglin <- lm(y ~ ., data = st)
+#prediz o valor do objeto
+round(predict(proj,object = reglin), 2)
+summary(reglin)
